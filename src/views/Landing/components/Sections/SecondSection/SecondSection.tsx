949f65f408be679d7b202e5bc567b3248f52c4f3
DESKTOP-U9NQFET\king_ <onrubia78@gmail.com>
2022-05-14 17:17:59 +0200

    feat(info): show some dashboard data into landing page

--- src/views/Landing/components/Sections/SecondSection/SecondSection.tsx
+++ src/views/Landing/components/Sections/SecondSection/SecondSection.tsx
@@ -1,5 +1,10 @@
 import { Box, Grid, Typography, useMediaQuery } from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
 import { styled } from "@material-ui/styles";
+import { useSelector } from "react-redux";
+import { IAppSlice } from "src/store/slices/app-slice";
+import { IReduxState } from "src/store/slices/state.interface";
+import { trim } from "src/helpers";
 
 const Container = styled(Box)({
     background: "#1D3473",
@@ -12,22 +17,27 @@ const StyledBox = styled(Box)({
     background: "#4469BD",
     boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
     textAlign: "center",
+    padding: "10px 0px",
 });
 
 const Title = styled(Typography)({
     fontFamily: "Montserrat Bold",
     fontSize: 24,
     color: "white",
-    padding: "10px 50px 10px 50px",
+    padding: "5px 50px 5px 50px",
 });
 const Subtitle = styled(Typography)({
     fontFamily: "Montserrat Medium",
     fontSize: 22,
     color: "white",
-    padding: "10px 50px 10px 50px",
+    padding: "5px 50px 5px 50px",
 });
 
 const SecondSection = () => {
+    const isAppLoading = useSelector<IReduxState, boolean>(state => state.app.loading);
+    const app = useSelector<IReduxState, IAppSlice>(state => state.app);
+    const trimmedStakingAPY = trim(app.stakingAPY * 100, 1);
+
     const isDesktop = useMediaQuery("(min-width:960px)");
 
     return (
@@ -35,15 +45,26 @@ const SecondSection = () => {
             <Box display="flex" flexDirection="column" px={2}>
                 <Grid container spacing={isDesktop ? 10 : 2}>
                     <Grid item xs={12} md={6}>
-                        <StyledBox>
-                            <Title>Treasury Balance</Title>
-                            <Subtitle>$4,537,292</Subtitle>
+                        <StyledBox minWidth={isDesktop ? 400 : "inherit"}>
+                            <Title>MAIA Price</Title>
+                            <Subtitle>{isAppLoading ? <Skeleton width="100px" /> : `$${trim(app.marketPrice, 2)}`}</Subtitle>
                         </StyledBox>
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <StyledBox>
+                        <StyledBox minWidth={isDesktop ? 400 : "inherit"}>
                             <Title>Treasury Balance</Title>
-                            <Subtitle>$4,537,292</Subtitle>
+                            <Subtitle>
+                                {isAppLoading ? (
+                                    <Skeleton width="250px" />
+                                ) : (
+                                    new Intl.NumberFormat("en-US", {
+                                        style: "currency",
+                                        currency: "USD",
+                                        maximumFractionDigits: 0,
+                                        minimumFractionDigits: 0,
+                                    }).format(app.stakingTVL)
+                                )}
+                            </Subtitle>
                         </StyledBox>
                     </Grid>
                 </Grid>
@@ -56,15 +77,26 @@ const SecondSection = () => {
                 </Grid>
                 <Grid container spacing={isDesktop ? 10 : 2}>
                     <Grid item xs={12} md={6}>
-                        <StyledBox>
-                            <Title>Treasury Balance</Title>
-                            <Subtitle>$4,537,292</Subtitle>
+                        <StyledBox minWidth={isDesktop ? 400 : "inherit"}>
+                            <Title>Market Cap</Title>
+                            <Subtitle>
+                                {isAppLoading ? (
+                                    <Skeleton width="160px" />
+                                ) : (
+                                    new Intl.NumberFormat("en-US", {
+                                        style: "currency",
+                                        currency: "USD",
+                                        maximumFractionDigits: 0,
+                                        minimumFractionDigits: 0,
+                                    }).format(app.marketCap)
+                                )}
+                            </Subtitle>
                         </StyledBox>
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <StyledBox>
-                            <Title>Treasury Balance</Title>
-                            <Subtitle>$4,537,292</Subtitle>
+                        <StyledBox minWidth={isDesktop ? 400 : "inherit"}>
+                            <Title>Current APY</Title>
+                            <Subtitle>{isAppLoading ? <Skeleton width="250px" /> : `${new Intl.NumberFormat("en-US").format(Number(trimmedStakingAPY))}%`}</Subtitle>
                         </StyledBox>
                     </Grid>
                 </Grid>
